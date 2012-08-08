b81aeb7835d09ad7bd60d6ac09c73d793fabcd58
Oscar Soderlund <oscar.d.soderlund@gmail.com>
2012-08-08 14:54:38 +0200

    implement navigation menu

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
+exports.index = function (req, res) {
   res.render('index', { title: 'Express' });
+};
+
+exports.section = function (req, res) {
+  res.render('section', { title: 'Express', section: {} });
 };
