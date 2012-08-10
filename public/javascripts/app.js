744e51654a43c2db5f084b81ad6a02504bf0a07f
Oscar Soderlund <oscar.d.soderlund@gmail.com>
2012-08-10 05:28:19 +0200

    refactor section form js and implement automatic color picker color change

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,12 +1,29 @@
 $(document).delegate('#section-form', 'pageinit', function () {
-  $('.color-picker')
-    .miniColors();
+  // Enhance color pickers
+  var $colorPickers = $('.color-picker');
+  $('input[name=color]')
+    .miniColors({
+      change: function (hex) {
+        $colorPickers.css('background', hex);
+      }
+    });
+  $('input[name=textColor]')
+    .miniColors({
+      change: function (hex) {
+        $colorPickers.css('color', hex);
+      }
+    });
+  // Init color picker colors
+  $colorPickers.css('background', $('input[name=color]').val());
+  $colorPickers.css('color', $('input[name=textColor]').val());
+  // Enhance file chooser
   $('input[type=file]')
     .textinput({
       theme: 'c'
     });
-  $('#upload-saint-image-true, #upload-saint-image-false')
+  // Radio button toggling of file chooser
+  $('input[name=uploadSaintImage]')
     .bind('change', function (event, ui) {
-      $('#saint-image').attr('disabled', $(this).val() === 'false');
-    });
+      $('input[name=saintImage]').attr('disabled', $(this).val() === 'false');
+    }).trigger('change');
 });
