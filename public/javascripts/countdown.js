ea3cadb0f0f58579c98e0303e0a5cdd1e8811249
Oscar Soderlund <oscar.d.soderlund@gmail.com>
2012-08-19 04:13:33 +0200

    implement countdown fullscreen transition

--- public/javascripts/countdown.js
+++ public/javascripts/countdown.js
@@ -6,7 +6,7 @@
 
     defaults = {
       message: '',
-      seconds: 10,
+      seconds: 13,
       expandEasing: 'easeOutCirc',
       shrinkEasing: 'easeInExpo',
       expandDuration: 200,
@@ -17,82 +17,118 @@
     methods = {
 
       init: function (options) {
-        console.log('found init');
-        console.log(this);
         return this.each(function () {
-          console.log('initing');
           var 
-            $this = $(this),
-            data = $this.data(namespace);
-          if (!data) {
-            console.log('initializing');
-            var
-              settings = $.extend(defaults, options),
-              $numberContainer = $('.cd-number', $this),
-              originalFontSize = $numberContainer.css('font-size');
-            $this.data(namespace, {
-              count: settings.seconds,
-              originalFontSize: originalFontSize,
-              settings: settings
-            });
-            methods.nextCount.call($this);
-          }
-        });
-      },
+            settings = $.extend(defaults, options),
+            $countdown = $(this),
+            $count = $('.cd-count', $countdown),
+            initialCount = (settings.seconds > 60 ?
+                           Math.ceil(settings.seconds / 60) * 60 :
+                           settings.seconds),
+            currCount = initialCount,
+            isFullscreen = false,
+            next = $.proxy($countdown.dequeue, $countdown);
 
-      nextCount: function () {
-        var 
-          data = this.data(namespace),
-          $numberContainer = $('.cd-number', this);
-        if (data.count > 0) {
-          console.log('counting');
-          $numberContainer
-            .text(data.count)
-            .css('font-size', 0)
-            .show(); 
-          data.count -= 1;
-          $numberContainer
-            .delay(200)
-            .animate({
-              'font-size': data.originalFontSize
-            }, {
-              duration: 700,
-              easing: data.settings.expandEasing
-            })
-            .delay(200)
-            .fadeOut(500, $.proxy(methods.nextCount, this));
-            // .animate({ 
-            //   'font-size': 0
-            // }, {
-            //   duration: data.settings.shrinkDuration,
-            //   easing: data.settings.shrinkEasing,
-            //   complete: $.proxy(methods.nextCount, this)
-            // });
-        } else {
-          data.settings.complete.call(this);
-        }
+          $count.css({
+            'font-size': $count.height() * 0.6
+          });
+
+          function nextCount() {
+            if (currCount < 1) return;
+            // Switch to fullscreen on last 10 seconds
+            if (!isFullscreen && currCount <= 10) {
+              $countdown
+                // Fade small overlay out
+                .fadeOut(1000)
+                .delay(200)
+                // Convert to big overlay
+                .queue(function () {
+                  $count
+                    .text('');
+                  $countdown
+                    .css({
+                      'padding': 0,
+                      'height': '100%',
+                      'width': '100%',
+                      'opacity': 1
+                    })
+                    .dequeue();
+                })
+                // Fade back in
+                .fadeIn(1000);
+              isFullscreen = true;
+            } 
+            if (currCount <= 10) {
+              $countdown
+                // Set the text
+                .queue(function () {
+                  $count
+                    .text(currCount)
+                    .css('font-size', 0)
+                    .show();
+                  $countdown.dequeue();
+                })
+                .delay(200)
+                // Expand the count
+                .queue(function () {
+                  $count
+                    .animate({
+                      'font-size': $count.height() / 2
+                    }, {
+                      duration: 700,
+                      easing: 'easeOutCirc',
+                      complete: function () {
+                        $countdown.dequeue();
+                      }
+                    });
+                })
+                .delay(200)
+                // Fade the count out
+                .queue(function () {
+                  $count.fadeOut(500, function () {
+                    $countdown.dequeue();
+                    currCount -= 1;
+                    nextCount();
+                  }); 
+                });
+            } else if (currCount <= 60) {
+              $count.text('Nollkampen börjar om ' + currCount + ' sekunder...')
+              currCount -= 1; 
+              $countdown
+                .delay(1000)
+                .queue(function () {
+                  $countdown.dequeue();
+                  nextCount();
+                });
+            } else {
+              $count.text('Nollkampen börjar om ' + Math.ceil(currCount / 60) + ' minuter...');
+              currCount -= 60;
+              $countdown
+                .delay(60000)
+                .queue(function () {
+                  $countdown.dequeue();
+                  nextCount();
+                });
+            }
+          }
+          nextCount();
+        });
       },
 
       destroy: function () {
         return this.each(function () {
-          var 
-            $this = $(this),
-            data = $this.data(namespace);
-          $this.stop(true);
-          $this.css('font-size', data.originalFontSize);
-          $this.text('');
-          $this.removeData(namespace);
+          $(this)
+            .clearQueue()
+            .text('');
         });
       }
 
     };
 
   $.fn.countdown = function (method) {
-    console.log('countdown');
     if (methods[method]) {
       return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
     } else if (typeof method === 'object' || !method) {
-      console.log('init going');
       return methods.init.apply(this, arguments);
     } else {
       $.error('Method' + method + 'does not exist on jQuery.' + namespace);
