21cdd0904ab3202d0644c52d6cfe66cf5f3eef4e
Oscar Soderlund <oscar.d.soderlund@gmail.com>
2012-08-21 13:56:30 +0200

    add firefox vibration support to sequence viewer

--- public/javascripts/sequence.js
+++ public/javascripts/sequence.js
@@ -3,9 +3,17 @@ $(function () {
   var $items = $('li');
   $items.hide();
   $items.first().css('display', 'block').show();
+  var currPos;
   var sequenceSwipe = new Swipe($sequence[0], {
     callback: function () {
-        navigator.vibrate(1000);
+      if (sequenceSwipe.getPos() !== currPos) {
+        navigator.mozVibrate([300, 100, 300]);
+      }
+      currPos = sequenceSwipe.getPos();
     }
+  });
+  currPos = sequenceSwipe.getPos();
+  $('*').bind('taphold', function () {
+    navigator.mozVibrate(1000);
   });
 });
